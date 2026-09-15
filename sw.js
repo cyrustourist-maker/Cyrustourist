73ec01b32daadf2804279a8e27eb637dd1dd9ea8
Cyrus Tourist <cyrustourist@gmail.com>
2026-09-15 11:36:03 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,3 +11,20 @@ const CORE_FILES = [
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
+    caches.open(CYRUS_CACHE).then(function (cache) {
+      return cache.addAll(CORE_FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (key) {
+            return key !== CYRUS_CACHE;
+          })
+          .map(function (key) {
+            return caches.delete(key);
